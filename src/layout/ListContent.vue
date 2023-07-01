9e9fc6c8b90d9833b75c23827fcfa9bc69f6e62f
GiovannyGarzon <giovankyandres@gmail.com>
2023-07-01 19:05:30 -0400

    updates

--- src/layout/ListContent.vue
+++ src/layout/ListContent.vue
@@ -7,32 +7,11 @@
         </div>
 
         <div class="list-content__main">
-            <div v-show="chapters" class="list-content__title-manga" v-for="chapter in chapters">
-                <img :src="`${chapter.manga.images.cover}`" class="list-content__image">
+            <div v-show="mangas" class="list-content__title-manga" v-for="manga in mangas">
+                <img :src="`${manga.images.cover}`" class="list-content__image">
                 <div class="list-content__info">
-                    <span class="list-content__name">{{ chapter.manga.title}}</span>
-                    <span class="list-content__author">{{ chapter.author.name }}</span>
-                </div>
-            </div>
-            <div v-show="chapters" ref="mangaTitle" class="list-content__title-manga" v-for="chapter in chapters">
-                <img ref="mangaImage" :src="`${chapter.manga.images.cover}`" class="list-content__image">
-                <div class="list-content__info">
-                    <span class="list-content__name">{{ chapter.manga.title}}</span>
-                    <span class="list-content__author">{{ chapter.author.name }}</span>
-                </div>
-            </div>
-            <div v-show="chapters" ref="mangaTitle" class="list-content__title-manga" v-for="chapter in chapters">
-                <img ref="mangaImage" :src="`${chapter.manga.images.cover}`" class="list-content__image">
-                <div class="list-content__info">
-                    <span class="list-content__name">{{ chapter.manga.title}}</span>
-                    <span class="list-content__author">{{ chapter.author.name }}</span>
-                </div>
-            </div>
-            <div v-show="chapters" ref="mangaTitle" class="list-content__title-manga" v-for="chapter in chapters">
-                <img ref="mangaImage" :src="`${chapter.manga.images.cover}`" class="list-content__image">
-                <div class="list-content__info">
-                    <span class="list-content__name">{{ chapter.manga.title}}</span>
-                    <span class="list-content__author">{{ chapter.author.name }}</span>
+                    <span class="list-content__name">{{ manga.title}}</span>
+                    <span class="list-content__author">{{ manga.author.name }}</span>
                 </div>
             </div>
         </div>
@@ -45,19 +24,19 @@ import { axios } from '../config'
 export default defineComponent({
     name: 'listContent',
     setup() {
-        const chapters = ref([])
+        const mangas = ref([])
 
-        const getChapters = async() => {
-            const { data } = await axios.get('/chapter')
-            chapters.value = data.data
+        const getMangas = async() => {
+            const { data } = await axios.get('/manga')
+            mangas.value = data.data
         }
 
         onMounted(() => {
-            getChapters()
+            getMangas()
         })
 
         return {
-            chapters,
+            mangas,
         }
     }
 })
