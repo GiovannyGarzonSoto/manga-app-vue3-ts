42e701fd7d2540d84dac962fa072df32d42bf73d
GiovannyGarzon <giovankyandres@gmail.com>
2023-07-03 14:55:18 -0400

    updates

--- src/layout/ListContent.vue
+++ src/layout/ListContent.vue
@@ -3,7 +3,7 @@
         <!-- TODO make a component -->
 
         <div class="list-content__search">
-            <input class="list-content__search-input input" maxlength="24" type="text" placeholder="Buscar por serie o autor">
+            <input class="list-content__search-input" maxlength="24" type="text">
         </div>
 
         <div class="list-content__main">
