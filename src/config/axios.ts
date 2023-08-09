a1a4eafb729affffd272880bdbaf9f58652f9d31
GiovannyGarzonSoto <giovankyandres@gmail.com>
2023-08-09 01:55:05 -0400

    scroll style

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: 'https://manga-app-express-mongo-production.up.railway.app/api',
-    baseURL: 'https://3088-200-83-84-50.ngrok-free.app/api'
+    baseURL: 'https://manga-app-express-mongo-production.up.railway.app/api',
+    // baseURL: 'https://3088-200-83-84-50.ngrok-free.app/api'
     // baseURL: 'http://localhost:3665/api'
 })
 
