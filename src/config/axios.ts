ac9807bd186dc564c4796b643856db071bf11b39
GiovannyGarzonSoto <giovankyandres@gmail.com>
2023-08-09 04:29:28 -0400

    .

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: 'https://manga-app-express-mongo-production.up.railway.app/api',
+    baseURL: 'https://manga-app-express-mongo-production.up.railway.app/api',
     // baseURL: 'https://3088-200-83-84-50.ngrok-free.app/api'
-    baseURL: 'http://localhost:3665/api'
+    // baseURL: 'http://localhost:3665/api'
 })
 
 export default instance
