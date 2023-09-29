b6045ed865922dba2d8b4937d358f0cf399432c6
GiovannyGarzonSoto <giovankyandres@gmail.com>
2023-09-29 05:05:16 -0300

    setup testing

--- test/unit/test.spec.ts
+++ test/unit/test.spec.ts
@@ -1,5 +1,9 @@
-describe('test test', () => {
-   test('test', () => {
-    
+import { shallowMount } from '@vue/test-utils'
+import Loader from '../../src/layout/Loader.vue'
+
+describe('test de prueba', () => {
+   test('probando componente', () => {
+      const wrapper = shallowMount(Loader)
+      expect(wrapper.html).toMatchSnapshot()
    })
 })
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    // rollupOptions: {
-    //   external: ['./public/menu-dots.svg', './public/logo.png', './public/menu.svg','./public/search.svg', './public/404.svg',
-    //   './public/books.svg', './public/check.svg', './public/menu.svg', './public/eye.svg', './public/comments.svg'],
-    // },
   },
   resolve: {
     alias: {
