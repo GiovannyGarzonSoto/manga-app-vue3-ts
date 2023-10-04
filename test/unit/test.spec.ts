7a944efb4b408806455f0232136457e057d58338
GiovannyGarzonSoto <giovankyandres@gmail.com>
2023-10-04 12:14:52 -0300

    vitest setup finish

--- test/unit/test.spec.ts
+++ test/unit/test.spec.ts
@@ -1,9 +1,11 @@
-import { shallowMount } from '@vue/test-utils'
 import Loader from '../../src/layout/Loader.vue'
+import {describe, expect, test} from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import { defineComponent } from 'vue'
 
 describe('test de prueba', () => {
    test('probando componente', () => {
       const wrapper = shallowMount(Loader)
-      expect(wrapper.html).toMatchSnapshot()
+      expect(wrapper.html()).toMatchSnapshot()
    })
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import vite_d_ts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-  },
+  plugins: [vue(), vite_d_ts()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+  optimizeDeps: {
+    exclude: ["vue"],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      exclude: ["src/**", "test/**"],
+      reporter:["json-summary", "html"]
+    },
+  },
+});
