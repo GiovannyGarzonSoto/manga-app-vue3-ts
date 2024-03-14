f39260d3068aefdeee7e950a2d3838a2b57ef677
GiovannyAndresGarzonSoto <giovannygarzonsoto@gmail.com>
2024-03-14 02:01:38 -0300

    changed URL axios manga api

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
     // baseURL: 'https://manga-app-express-mongo-production.up.railway.app/api',
     // baseURL: 'https://3088-200-83-84-50.ngrok-free.app/api'
-    baseURL: 'http://localhost:3665/api'
+    baseURL: 'http://143.198.60.56:3665/api'
 })
 
 export default instance
